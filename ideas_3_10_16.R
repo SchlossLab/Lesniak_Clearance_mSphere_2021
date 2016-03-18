a6b6a5d0bba33ac07449ebd13f34a2211cb10985
Kaitlin <kjflynn06@gmail.com>
2016-03-18 16:40:35 -0400

    added simp diversity plot code woo

--- ideas_3_10_16.R
+++ ideas_3_10_16.R
@@ -1,31 +1,8 @@
 alyxs project
 
--inverse simpson, diversity over time, average per cage and time on x
-  summary.single, read into R, combine
-  
--think of hypotheses first 
-
-  fxn for cage # and days avail   
-  
-  lost of cages and days save to lists 
-  series of for loops
-  
-  do abx over that
-  write a function that takes in treatment group, cage num and days in the cage
-  that will then loop through and take simpson mean of each sample in the cage per day
-  and either plot that for each day, or save it to a list that we can then plot
-  
-  treatment group > cages >days 
-  either plot right away or save to lists 
-  
 -look at community over time vs cdiff over time
 
 -abundance things 
-  
--then go to dynamics and stuff 
-  go back and look at stein paper 
-  get table of abundance and how much/if they chance
-  rank abundance 
   
 could do machine learning and classify if cdiff stuck around or not, if they were  susceptible community
 do feature selection and see what bugs are susceptible/permissible 
--- shannon_diversity.R
+++ shannon_diversity.R
@@ -17,10 +17,10 @@ simpson <- read.table(file = "data/mothur/abx_time.trim.contigs.good.unique.good
 simpgroup <- simpson[, colnames(simpson) %in% c("group", "invsimpson")]
 fullSimpson <- merge(metadata, simpgroup)
 
-
+#this will make a diversity plot for each treatment that averages the mice in each cage and plots diversity per cage over time 
 for(treatment in levels(fullSimpson$abx)){
   plot(0, type='n', ylim=c(0,20), xlim=c(-6,10), xlab='day', ylab='inverse simpson', main=treatment)
-  colors <- c('red','blue','green','pink','purple', 'orange')
+  colors <- c('red','blue','green','pink','purple', 'orange', 'brown', 'black', 'yellow')
   cages <- unique(fullSimpson[fullSimpson$abx==treatment, 'cage'])
   for(j in 1:length(cages)){
     days <- sort(unique(fullSimpson[fullSimpson$abx==treatment & fullSimpson$cage==cages[j],'day']))
@@ -28,9 +28,57 @@ for(treatment in levels(fullSimpson$abx)){
     for(i in 1:length(days)){
       avg_simp[i] <- mean(fullSimpson[fullSimpson$abx==treatment & fullSimpson$cage==cages[j] & fullSimpson$day==days[i], 'invsimpson'])
     }
-    points(days, avg_simp, type='b', col=colors[j])
+      points(days, avg_simp, type='b', col=colors[j])
+      
   }
-  legend("top", legend=cages, col=colors, pch=1, lty=1, horiz=T)
+  legend("top", legend=cages, col=colors, cex=0.7, pch=1, lty=1, horiz=T)
 } 
-    
-    
+ 
+
+#to make a plot that will avg all of the mice together in a treatment and plot over time lets do this   
+
+
+
+#Or the ddply way. turn this into a script/loop this weekend, dont forget to add SD lines
+#test <- ddply(fullSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+
+library(plyr)
+plot(0, type='n', ylim=c(0,25), xlim=c(0,10), xlab='day', ylab='inverse simpson', main='Diversity over time, all treatments')
+
+clindaSimpson <- subset(fullSimpson, abx == 'clinda')
+strepSimpson <- subset(fullSimpson, abx == 'strep')
+ampSimpson <- subset(fullSimpson, abx == 'amp')
+cefSimpson <- subset(fullSimpson, abx == 'cef')
+cipSimpson <- subset(fullSimpson, abx == 'cipro')
+metSimpson <- subset(fullSimpson, abx == 'metro')
+vancSimpson <- subset(fullSimpson, abx == 'vanc')
+
+avg_clin_simp <- ddply(clindaSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+avg_strep_simp <- ddply(strepSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+avg_amp_simp <- ddply(ampSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+avg_cef_simp <- ddply(cefSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+avg_cip_simp <- ddply(cipSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+avg_met_simp <- ddply(metSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+avg_vanc_simp <- ddply(vancSimpson,~day,summarise, avg = mean(invsimpson), sd = sd(invsimpson))
+
+points(avg_strep_simp$day, avg_strep_simp$avg, type = 'b',lwd = 2,  col = 'red')
+points(avg_clin_simp$day, avg_clin_simp$avg, type = 'b',lwd = 2,  col = 'orange')
+points(avg_amp_simp$day, avg_amp_simp$avg, type = 'b', lwd = 2, col = 'yellow')
+points(avg_cef_simp$day, avg_cef_simp$avg, type = 'b', lwd = 2, col = 'green')
+points(avg_cip_simp$day, avg_cip_simp$avg, type = 'b', lwd = 2, col = 'blue')
+points(avg_met_simp$day, avg_met_simp$avg, type = 'b', lwd = 2, col = 'purple')
+points(avg_vanc_simp$day, avg_vanc_simp$avg, type = 'b', lwd = 2, col = 'brown')
+
+legend("top", legend=c('strep', 'clinda', 'amp', 'cef', 'cipro', 'metro', 'vanc'), col=c('red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown'), cex=0.7, pch=1, lty=1, lwd = 2, horiz=T)
+
+arrows(avg_vanc_simp$day, avg_vanc_simp$avg-avg_vanc_simp$sd, avg_vanc_simp$day, avg_vanc_simp$avg+avg_vanc_simp$sd, length=0.05, angle=90, code=3, col = 'brown')
+arrows(avg_strep_simp$day, avg_strep_simp$avg-avg_strep_simp$sd, avg_strep_simp$day, avg_strep_simp$avg+avg_strep_simp$sd, length=0.05, angle=90, code=3, col = 'red')
+arrows(avg_clin_simp$day, avg_clin_simp$avg-avg_clin_simp$sd, avg_clin_simp$day, avg_clin_simp$avg+avg_clin_simp$sd, length=0.05, angle=90, code=3, col = 'orange')
+arrows(avg_amp_simp$day, avg_amp_simp$avg-avg_amp_simp$sd, avg_amp_simp$day, avg_amp_simp$avg+avg_amp_simp$sd, length=0.05, angle=90, code=3, col = 'yellow')
+arrows(avg_cef_simp$day, avg_cef_simp$avg-avg_cef_simp$sd, avg_cef_simp$day, avg_cef_simp$avg+avg_cef_simp$sd, length=0.05, angle=90, code=3, col = 'green')
+arrows(avg_cip_simp$day, avg_cip_simp$avg-avg_cip_simp$sd, avg_cip_simp$day, avg_cip_simp$avg+avg_cip_simp$sd, length=0.05, angle=90, code=3, col = 'blue')
+arrows(avg_met_simp$day, avg_met_simp$avg-avg_met_simp$sd, avg_met_simp$day, avg_met_simp$avg+avg_met_simp$sd, length=0.05, angle=90, code=3, col = 'purple')
+
+
+#now to make the OTU table do this
+otu_table <- read.table(file = "data/mothur/genus.1.subsample.shared.trimmed", header = T)
