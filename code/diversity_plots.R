eb70801e6724db13de776f0f30cf582e1117afd8
Nick Lesniak <NLesniak@umich.edu>
2017-12-14 13:46:28 -0500

    add diversity_plots.R

---
+++ code/diversity_plots.R
@@ -0,0 +1,16 @@
+# diversity vs colonization
+
+#alpha diversity
+communities colonized to higher levels have lower diversity (alpha)
+	association between cfu and alpha
+	cfu vs # of otus
+	shared otus?
+#beta diversity
+highly infected communities are most different than untreated
+	separation between untreated mice and all the highly infected communities (>1e6)
+communities that recover/elimnate cdifficile are more diverse
+	difference in diversity between highly infected 
+more change w/low diversity?
+more change with high cfu?
+
+need to remove dependence of daily sampling?
