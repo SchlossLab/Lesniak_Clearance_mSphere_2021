ff547d31569fe005af424aed511341dea5f19a11
Nick Lesniak <NLesniak@umich.edu>
2018-01-04 12:58:59 -0500

    ideas added

--- ideas_3_10_16.R
+++ ideas_3_10_16.R
@@ -15,3 +15,30 @@ get U grant
 
 
 
+12_13_17
+
+What is my quesiton? -Nick
+
+association between microbes and level?
+	day x to day x?
+	day x to day x+1?
+predict colonization level 
+	from day 0?
+	from prior day?
+communities colonized to higher levels have lower diversity (alpha)
+	association between cfu and alpha
+highly infected communities are most different than untreated
+	separation between untreated mice and all the highly infected communities (>1e6)
+communities that recover/elimnate cdifficile are more diverse
+	difference in diversity between highly infected 
+each community (abx) will have a unique network
+	by correlation
+	by ML - RF will perform best when applied to a single treatment
+
+how does diversity affect colonization
+	cfu vs # of otus
+	cfu vs rel abun
+shared otus?
+beta diversity
+	more change w/low diversity?
+	more change with high cfu?
