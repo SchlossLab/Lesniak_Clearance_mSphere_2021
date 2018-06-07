6baa26a594cc21e328cc3a3c9624733028fcdc8f
Nick Lesniak <NLesniak@umich.edu>
2018-06-07 14:26:55 -0400

    map to read in all output files

--- code/ccm_network_analysis.R
+++ code/ccm_network_analysis.R
@@ -2,6 +2,22 @@ library(tidyverse)
 
 library(cowplot)
 
+data_path <- "scratch/ccm_all/"   # path to the data
+files <- dir(data_path, pattern = "ccm_raw_data*") # get file names
+treatment_list <- unique(gsub('\\d{,2}.txt', '', files))
+
+for(treatment in treatment_list){
+	
+	files <- dir(data_path, pattern = paste0(treatment, "*")) # get file names
+	data <- files %>%
+	  # read in all the files, appending the path before the filename
+	  map(~ read.table(file.path(data_path, .), header = T, stringsAsFactors = F)) %>% 
+	  reduce(rbind)
+
+}
+
+
+
 significant_output <- ccm_output %>% 
 	filter(pval_b_cause_a < 0.05 | pval_a_cause_b < 0.05) %>% 
 	unite(treatment, abx, dose, delayed_infection, otu) %>% 
