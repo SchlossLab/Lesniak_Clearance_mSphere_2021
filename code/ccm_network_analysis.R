05b07bf411156a185b60461fe560ff59eff5450e
Nick Lesniak <NLesniak@umich.edu>
2018-06-07 14:28:02 -0400

    begin setting up dataframe

--- code/ccm_network_analysis.R
+++ code/ccm_network_analysis.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(statnet)
 library(cowplot)
 
 data_path <- "scratch/ccm_all/"   # path to the data
@@ -13,6 +13,54 @@ for(treatment in treatment_list){
 	  # read in all the files, appending the path before the filename
 	  map(~ read.table(file.path(data_path, .), header = T, stringsAsFactors = F)) %>% 
 	  reduce(rbind)
+
+test_data <- data %>% 
+	filter(otu1 %in% c('CFU', 'Otu000004', 'Otu000008'), 
+		otu2 %in% c('CFU', 'Otu000004', 'Otu000008')) %>% 
+test_data %>% 
+	select(otu1, otu2, otu1_cause_otu2, otu2_cause_otu1) %>% 
+	gather(interaction, strength, otu1_cause_otu2, otu2_cause_otu1) %>% 
+	separate(interaction, c('causal_otu', 'unused', 'affected_otu')) %>% 
+	mutate(causal_otu = case_when(causal_otu == 'otu1' ~ otu1,
+			causal_otu == 'otu2' ~ otu2,
+			TRUE ~ 'Error'),
+		affected_otu = case_when(affected_otu == 'otu1' ~ otu1,
+			affected_otu == 'otu2' ~ otu2,
+			TRUE ~ 'Error')) %>% 
+	group_by(causal_otu, affected_otu) %>% 
+	summarise(strength = median(strength)) 
+test_test <- data.frame(otu1 = rep(c('Ot1','Ot2','Ot3'), 3),
+	otu2 = rep(c('Ot1','Ot2','Ot3'), each = 3),
+	otu1_otu2 = sample(100, 9),
+	otu2_otu1 = sample(100, 9), stringsAsFactors = F) %>% 
+	gather(interaction, strength, otu1_otu2, otu2_otu1) %>% 
+	separate(interaction, c('causal_otu', 'affected_otu')) %>% 
+	
+	ccm_prediction_slope <- bind_rows(
+		select(data, otu = otu1, prediction_slope = otu1_prediction_slope, p_slope = otu1_prediction_slope_p, E = E_A),
+		select(data, otu = otu2, prediction_slope = otu2_prediction_slope, p_slope = otu2_prediction_slope_p, E = E_B))
+
+	interaction_data <- data %>% 
+		group_by(otu1, otu2) %>% 
+		summarise(med_p_a_b = median(pval_a_cause_b),
+			med_p_b_a = median(pval_b_cause_a),
+			otu1_cause_otu2 = median(otu1_cause_otu2),
+			otu2_cause_otu1 = median(otu2_cause_otu1)) %>% 
+		gather(pval_comparison, pvalue, contains('med_p')) %>% 
+		gather(interaction, strength, contains('cause')) %>% 
+		#arrange(desc(otu1), desc(otu2)) %>%
+		mutate(adj_strength = ifelse(pvalue > 0.05, 0, strength)) 
+
+	interaction_data %>% 
+		ggplot(aes(otu2, otu1)) + geom_tile(aes(fill = adj_strength)) + 
+			scale_fill_gradient(low = 'white', high = 'blue')
+
+test_data <- interaction_data %>% 
+	filter(otu1 %in% c('Otu000001', 'Otu000003', 'Otu000004'), 
+		otu2 %in% c('Otu000001', 'Otu000003', 'Otu000004'))
+
+	num_nodes <- length(unique(data$otu1))
+	network_matrix <- matrix()
 
 }
 
