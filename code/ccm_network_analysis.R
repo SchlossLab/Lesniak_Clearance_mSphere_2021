49826b8490632e17d0db4881d5ce3437fc541843
Nick Lesniak <NLesniak@umich.edu>
2018-06-20 15:29:02 -0400

    convert plots to if statement in case no significant interactions

--- code/ccm_network_analysis.R
+++ code/ccm_network_analysis.R
@@ -29,9 +29,9 @@ for(treatment in treatment_list){
 	  reduce(rbind)
 
 	input_data <- bind_rows(
-		select(input_data, otu = otu1, strength = otu1_cause_otu2, p_value = pval_a_cause_b, affected_otu = otu2,
+		select(input_data, otu = otu1, strength = otu1_cause_otu2, p_value = pval_otu1_casue_otu2, affected_otu = otu2,
 			prediction_slope = otu1_prediction_slope, p_slope = otu1_prediction_slope_p, E = E_A),
-		select(input_data, otu = otu2, strength = otu2_cause_otu1, p_value = pval_b_cause_a, affected_otu = otu1,
+		select(input_data, otu = otu2, strength = otu2_cause_otu1, p_value = pval_otu2_casue_otu1, affected_otu = otu1,
 			prediction_slope = otu2_prediction_slope, p_slope = otu2_prediction_slope_p, E = E_B))
 
 #	taxonomic_labels <- get_taxa_labels(taxa_file = taxonomy_file,  taxa_level='genus', 
@@ -63,99 +63,100 @@ for(treatment in treatment_list){
 					gsub('([[:alpha:]]+_){3}|_[[:alpha:]]+$', '', treatment)),
 					x = 'Driver OTU', y = 'Driven OTU'),
 		width = 12, height = 12)
+	if(sum(interaction_data$adj_strength) > 0){
+		num_nodes <- length(unique(input_data$otu))
+		network_matrix <- matrix(interaction_data$adj_strength,
+			nrow = num_nodes, ncol = num_nodes)
+		diag(network_matrix) <- 0
 
-	num_nodes <- length(unique(input_data$otu))
-	network_matrix <- matrix(interaction_data$adj_strength,
-		nrow = num_nodes, ncol = num_nodes)
-	diag(network_matrix) <- 0
+		network <- as.network(x = network_matrix,
+			directed = T,
+			loops = F,
+			matrix.type = 'adjacency')
 
-	network <- as.network(x = network_matrix,
-		directed = T,
-		loops = F,
-		matrix.type = 'adjacency')
+		network.vertex.names(network) <- interaction_data$driver_taxa
 
-	network.vertex.names(network) <- interaction_data$driver_taxa
+		#plot.network(network, # our network object
+	    #         vertex.col = 'gray', # color nodes by gender
+	    #         vertex.cex = 2, # size nodes by their age
+	    #         displaylabels = T, # show the node names
+	    #         label.pos = 5 # display the names directly over nodes
+	    #         )
 
-	#plot.network(network, # our network object
-    #         vertex.col = 'gray', # color nodes by gender
-    #         vertex.cex = 2, # size nodes by their age
-    #         displaylabels = T, # show the node names
-    #         label.pos = 5 # display the names directly over nodes
-    #         )
+	    interacting_otus <- interaction_data %>% 
+	    	filter(adj_strength > 0) %>% 
+	    	gather(role, taxa, driver_taxa, driven_taxa) %>% 
+	    	pull(taxa) %>% 
+	    	unique
+		gg_network_data <- data.frame(from_id = interaction_data$driver_taxa,
+			to_id = interaction_data$driven_taxa) %>% 
+			mutate(to_id = ifelse(interaction_data$adj_strength == 0, NA, interaction_data$driven_taxa)) %>% 
+			filter(from_id %in% interacting_otus) %>% 
+			mutate(from_id = gsub(' \\(', '\n(', from_id),
+				to_id = gsub(' \\(', '\n(', to_id),)
 
-    interacting_otus <- interaction_data %>% 
-    	filter(adj_strength > 0) %>% 
-    	gather(role, taxa, driver_taxa, driven_taxa) %>% 
-    	pull(taxa) %>% 
-    	unique
-	gg_network_data <- data.frame(from_id = interaction_data$driver_taxa,
-		to_id = interaction_data$driven_taxa) %>% 
-		mutate(to_id = ifelse(interaction_data$adj_strength == 0, NA, interaction_data$driven_taxa)) %>% 
-		filter(from_id %in% interacting_otus) %>% 
-		mutate(from_id = gsub(' \\(', '\n(', from_id),
-			to_id = gsub(' \\(', '\n(', to_id),)
+		## Using Name1 as the from node column and Name2 as the to node column.
+		## If this is not correct, rewrite dat so that the first 2 columns are from and to node, respectively.
 
-	## Using Name1 as the from node column and Name2 as the to node column.
-	## If this is not correct, rewrite dat so that the first 2 columns are from and to node, respectively.
+		## Joining edge and node information by from_id and label respectively.
 
-	## Joining edge and node information by from_id and label respectively.
-
-	# create plot
-	set.seed(1)
-	ggsave(paste0(save_dir, current_treatment, '_network.jpg'),
-		ggplot(data = gg_network_data, aes(from_id = from_id, to_id = to_id)) +
-			geom_net(layout.alg = "kamadakawai", 
-				size = 2, labelon = TRUE, vjust = -0.6, ecolour = "grey60",
-				directed = TRUE, fontsize = 3, ealpha = 0.5) +
-			xlim(c(-0.05, 1.05)) +
-			theme_net() +
-			theme(legend.position = "bottom"),
-		width = 10, height = 10)
-
-
-	#interacting_otus <- taxonomic_labels %>% 
-	#	filter(tax_otu_label %in% interacting_otus) %>% 
-	#	pull(otu) %>% 
-	#	c(., 'CFU')
-	interacting_otus <- unique(gg_network_data$from_id)
-
-	abx_df <- meta_df %>% 
-		select(group, cage, mouse, day, C_difficile=CFU, cdiff, abx, dose, delayed) %>% 
-		unite(treatment, abx, dose, delayed) %>% 
-		filter(cdiff == T, day >= 0, treatment == current_treatment) %>% 
-		mutate(unique_id = paste(cage, mouse, sep = '_')) %>% 
-		inner_join(select(shared_by_genus, one_of(c(interacting_otus, 'Group'))),
-			by = c('group' = "Group"))
+		# create plot
+		set.seed(1)
+		ggsave(paste0(save_dir, current_treatment, '_network.jpg'),
+			ggplot(data = gg_network_data, aes(from_id = from_id, to_id = to_id)) +
+				geom_net(layout.alg = "kamadakawai", 
+					size = 2, labelon = TRUE, vjust = -0.6, ecolour = "grey60",
+					directed = TRUE, fontsize = 3, ealpha = 0.5) +
+				xlim(c(-0.05, 1.05)) +
+				theme_net() +
+				theme(legend.position = "bottom"),
+			width = 10, height = 10)
 
 
-	otu_temporal_plot <- abx_df %>% 
-		select(cage, mouse, day, one_of(interacting_otus)) %>% 
-		gather(bacteria, counts, one_of(interacting_otus)) %>% 
-			ggplot(aes(x = day, y = counts, color = interaction(as.factor(mouse), as.factor(cage)), 
-				group = interaction(cage, mouse))) + 
-				geom_line() + 
-				facet_grid(bacteria~., scales = 'free_y') +
-				theme_bw() + 
-				labs(x = 'Day', y = 'Abundance \n (C difficle = CFU, Otu = 16s counts)', 
-					title = 'Temporal Dynamics', subtitle = 'Colored by mouse') + 
-				scale_x_continuous(breaks=seq(0,10, 1)) + 
-				theme_bw(base_size = 8) + 
-				theme(legend.position = 'none')
-	cdiff_temporal_plot <- abx_df %>% 
-		select(cage, mouse, day, C_difficile) %>% 
-			ggplot(aes(x = day, y = C_difficile + 1, color = interaction(as.factor(mouse), as.factor(cage)), 
-				group = interaction(cage, mouse))) + 
-				geom_line() + scale_y_log10() + 
-				theme_bw() + 
-				labs(x = 'Day', y = 'Abundance \n (C difficle = CFU)', 
-					title = 'Temporal Dynamics', subtitle = 'Colored by mouse') + 
-				scale_x_continuous(breaks=seq(0,10, 1)) + 
-				theme_bw(base_size = 8) + 
-				theme(legend.position = 'none')
+		#interacting_otus <- taxonomic_labels %>% 
+		#	filter(tax_otu_label %in% interacting_otus) %>% 
+		#	pull(otu) %>% 
+		#	c(., 'CFU')
+		interacting_otus <- unique(gg_network_data$from_id)
+
+		abx_df <- meta_df %>% 
+			select(group, cage, mouse, day, C_difficile=CFU, cdiff, abx, dose, delayed) %>% 
+			unite(treatment, abx, dose, delayed) %>% 
+			filter(cdiff == T, day >= 0, treatment == current_treatment) %>% 
+			mutate(unique_id = paste(cage, mouse, sep = '_')) %>% 
+			inner_join(select(shared_by_genus, one_of(c(interacting_otus, 'Group'))),
+				by = c('group' = "Group"))
 
 
-	ggsave(paste0(save_dir, current_treatment, '_dynamics.jpg'),
-		otu_temporal_plot + cdiff_temporal_plot + plot_layout(ncol = 1),
-		width = 10, height = 20)
+		otu_temporal_plot <- abx_df %>% 
+			select(cage, mouse, day, one_of(interacting_otus)) %>% 
+			gather(bacteria, counts, one_of(interacting_otus)) %>% 
+				ggplot(aes(x = day, y = counts, color = interaction(as.factor(mouse), as.factor(cage)), 
+					group = interaction(cage, mouse))) + 
+					geom_line() + 
+					facet_grid(bacteria~., scales = 'free_y') +
+					theme_bw() + 
+					labs(x = 'Day', y = 'Abundance \n (C difficle = CFU, Otu = 16s counts)', 
+						title = 'Temporal Dynamics', subtitle = 'Colored by mouse') + 
+					scale_x_continuous(breaks=seq(0,10, 1)) + 
+					theme_bw(base_size = 8) + 
+					theme(legend.position = 'none')
+		cdiff_temporal_plot <- abx_df %>% 
+			select(cage, mouse, day, C_difficile) %>% 
+				ggplot(aes(x = day, y = C_difficile + 1, color = interaction(as.factor(mouse), as.factor(cage)), 
+					group = interaction(cage, mouse))) + 
+					geom_line() + scale_y_log10() + 
+					theme_bw() + 
+					labs(x = 'Day', y = 'Abundance \n (C difficle = CFU)', 
+						title = 'Temporal Dynamics', subtitle = 'Colored by mouse') + 
+					scale_x_continuous(breaks=seq(0,10, 1)) + 
+					theme_bw(base_size = 8) + 
+					theme(legend.position = 'none')
+
+
+		ggsave(paste0(save_dir, current_treatment, '_dynamics.jpg'),
+			otu_temporal_plot + cdiff_temporal_plot + plot_layout(ncol = 1),
+			width = 10, height = 20)
+	}
 
 }
