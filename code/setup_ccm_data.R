beb5c0b67cc7d90dd55ae0184a471f19ea6541fa
Nick Lesniak <NLesniak@umich.edu>
2018-08-22 17:16:09 -0400

    remove otus by treatment in data setup

--- code/setup_ccm_data.R
+++ code/setup_ccm_data.R
@@ -15,45 +15,53 @@ ccm_df <- meta_file %>%
 	mutate(unique_id = paste(cage, mouse, sep = '_')) %>% 
 	inner_join(shared_file, by = c('group' = "Group")) %>% 
 	select(-group, -numOtus) %>% 
-	rename(C_difficile = CFU)
-		
-# remove otus that are present in less than 10 samples
-ccm_df <- select(ccm_df, day, C_difficile, which(apply(ccm_df > 1, 2, sum) > 10 ))  
+	rename(C_difficile = CFU) %>% 
+	gather(taxa, raw_abundance, C_difficile, contains('Otu00'))
 
+		
+
+taxa_by_treatment <- ccm_df %>% 
+	group_by(treatment, taxa) %>% 
+	# remove otus that are present in less than 10 samples for each treatment	
+	mutate(present = sum(raw_abundance > 1) > 10 ) %>% 
+	ungroup() %>% 
+	filter(present == T) %>% 
+	# fill in missing days
+	select(unique_id, treatment, taxa, day) %>% 
+	mutate(present = T) %>% 
+	spread(day, present) %>% 
+	gather(day, present, -unique_id, -treatment, -taxa) %>% 
+	select(-present) %>% 
+	mutate(day = as.numeric(day))
+
+# normalize each taxa for each subject
 normalize <- function(x, ...) {
     (x - mean(x, ...))/sd(x, ...)
 }
-
 ccm_df <- ccm_df %>% 
-	# replace all 0s with random value between 0 and 1
-	gather(taxa, abundance, C_difficile, contains('Otu00')) %>% 
-	mutate(abundance = ifelse(abundance == 0, 
-		sample(100, sum(abundance == 0), replace = T)/100, abundance)) %>% 
-	spread(taxa, abundance) %>% 
-	# add missing days
-	full_join(data.frame(
-		unique(select(ccm_df, unique_id, treatment))[
-			rep(1:nrow(unique(select(ccm_df, unique_id, treatment))), each = 11), ],
-		day = rep(seq(0,10), length(unique(ccm_df$unique_id))),
-		stringsAsFactors = F), by = c('unique_id', 'day', 'treatment')) %>% 
+	# add missing days and only use specific otus per treatment group
+	right_join(taxa_by_treatment, by = c('unique_id', 'day', 'treatment', 'taxa')) %>% 
 	# impute values for missing days and normalize by mouse
-	arrange(unique_id, day) %>% 
-	gather(bacteria, raw_abundance, C_difficile, contains('Otu00')) %>% 
-	group_by(unique_id, bacteria) %>% 
+	arrange(unique_id, taxa, day) %>% 
+	group_by(unique_id, taxa) %>% 
 	nest() %>% 
 	mutate(abundance = map(data, ~normalize(na.interp(.$raw_abundance)))) %>% 
 	unnest(data, abundance) %>% 
-	select(treatment, unique_id, day, bacteria, abundance) 
+# dont think i need this since im not using mccm and now using z-score normalized data
+#	# replace all 0s with random value between 0 and 1
+#	mutate(abundance = ifelse(abundance == 0, 
+#		sample(100, sum(abundance == 0), replace = T, na.rm = T)/100, abundance)) %>% 
+	select(treatment, unique_id, day, taxa, abundance) 
 
 ccm_df <- ccm_df %>% 
-	arrange(unique_id, bacteria, day) %>% 
-	group_by(unique_id, bacteria) %>% 
+	arrange(unique_id, taxa, day) %>% 
+	group_by(unique_id, taxa) %>% 
 	mutate(first = abundance - lag(abundance),
 		second = abundance - lag(abundance, 2)) %>% 
 	ungroup %>% 
 	rename(raw = abundance) %>% 
 	gather(differenced, abundance, raw, first, second) %>% 
-	mutate(variable = paste0(bacteria, '_', differenced))
+	mutate(variable = paste0(taxa, '_', differenced))
 
 write.table(ccm_df, paste0(save_dir, 'ccm_otu_data.txt'), 
-	quote = F, row.names = F)
+	quote = F, row.names = F,)
