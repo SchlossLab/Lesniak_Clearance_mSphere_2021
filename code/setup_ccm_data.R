20ca142057c1e20847c810572396789be728728f
Nick Lesniak <NLesniak@umich.edu>
2018-08-29 14:44:55 -0400

    z transform by otu/cfu for whole data set

--- code/setup_ccm_data.R
+++ code/setup_ccm_data.R
@@ -11,12 +11,19 @@ meta_file   <- read.table(meta_file, sep = '\t', header = T, stringsAsFactors = 
 shared_file <- 'data/mothur/abx_time.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.0.03.subsample.shared'
 shared_file <- read.table(shared_file, sep = '\t', header = T, stringsAsFactors = F)
 
+# normalize each taxa for each subject
+normalize <- function(x, ...) {
+	(x - mean(x, ...))/sd(x, ...)
+}
+
 ccm_df <- meta_file %>% 
 	mutate(unique_id = paste(cage, mouse, sep = '_')) %>% 
 	inner_join(shared_file, by = c('group' = "Group")) %>% 
-	select(-group, -numOtus) %>% 
-	rename(C_difficile = CFU) %>% 
-	gather(taxa, raw_abundance, C_difficile, contains('Otu00'))
+	select(-group, -numOtus) %>%
+	rename(C_difficile = CFU) %>%  
+	gather(taxa, raw_abundance, C_difficile, contains('Otu00')) %>% 
+	group_by(taxa) %>% 
+	mutate(normalized_abundance = normalize(raw_abundance)) 
 
 taxa_by_treatment <- ccm_df %>% 
 	group_by(treatment, taxa) %>% 
@@ -32,39 +39,25 @@ taxa_by_treatment <- ccm_df %>%
 	select(-present) %>% 
 	mutate(day = as.numeric(day))
 
-# normalize each taxa for each subject
-normalize <- function(x, ...) {
-	(x - mean(x, ...))/sd(x, ...)
-}
-
-#ccm_df <- 
-ccm_df %>% 
+ccm_df <- ccm_df %>% 
 	# add missing days and only use specific otus per treatment group
 	right_join(taxa_by_treatment, by = c('unique_id', 'day', 'treatment', 'taxa')) %>% 
 	# impute values for missing days and normalize by mouse
 	arrange(unique_id, taxa, day) %>% 
 	group_by(unique_id, taxa) %>% 
 	nest() %>% 
-	mutate(abundance = map(data, ~normalize(na.interp(.$raw_abundance)))) %>% 
-	unnest(data, abundance) %>% 
-# dont think i need this since im not using mccm and now using z-score normalized data
-#	# replace all 0s with random value between 0 and 1
-#	mutate(abundance = ifelse(abundance == 0, 
-#		sample(100, sum(abundance == 0), replace = T, na.rm = T)/100, abundance)) %>% 
-	select(treatment, unique_id, day, taxa, abundance) 
-
-# replace NAs with 0
-ccm_df <- ccm_df %>% 
-	mutate(abundance = ifelse(is.na(abundance), 0, abundance))
+	mutate(normalized_abundance = map(data, ~na.interp(.$normalized_abundance))) %>% 
+	unnest(data, normalized_abundance) %>% 
+	select(treatment, unique_id, day, taxa, normalized_abundance, raw_abundance)
 
 ccm_df <- ccm_df %>% 
 	arrange(unique_id, taxa, day) %>% 
 	group_by(unique_id, taxa) %>% 
-	mutate(first = abundance - lag(abundance),
-		second = abundance - lag(abundance, 2)) %>% 
+	mutate(first = normalized_abundance - lag(normalized_abundance),
+		second = normalized_abundance - lag(normalized_abundance, 2)) %>% 
 	ungroup %>% 
-	rename(raw = abundance) %>% 
-	gather(differenced, abundance, raw, first, second) %>% 
+	rename(undifferenced = normalized_abundance) %>% 
+	gather(differenced, normalized_abundance, undifferenced, first, second) %>% 
 	mutate(otu_feature = paste0(taxa, '_', differenced))
 
 write.table(ccm_df, paste0(save_dir, 'ccm_otu_data.txt'), 
