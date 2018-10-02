3d5b88fa7435208fd0da4a07912e3ca193beff03
Nick Lesniak <NLesniak@umich.edu>
2018-10-02 14:09:42 -0400

    check for completed ccm runs and skip completed set
    also moved seed into run to eliminate differences due to how many sets have been run

--- code/ccm_analysis.R
+++ code/ccm_analysis.R
@@ -55,142 +55,148 @@ mouse_list <- unique(abx_df$unique_id)
 # create a list of all combinations of taxa
 otu_combinations <- apply(combinations(length(taxa_list), 2, repeats=TRUE), 1, list)
 
-set.seed(seed)
 
 n_samples <- nrow(filter(abx_df, otu_feature == unique(embedding_nonlinearity$taxa)[1]))
 lib_sizes <- c(seq(5, n_samples, by = 5))
 
 run_ccm <- function(otu, input_df, treatment_subset, taxa_list){
+	set.seed(seed)
 	# current_otu1 <- taxa_list[ 1 ]
 	# current_otu2 <- taxa_list[ 1 ]
 	current_otu1 <- taxa_list[ otu[[1]][1] ]
 	current_otu2 <- taxa_list[ otu[[1]][2] ]
-	
-	# input_df <- abx_df
-	composite_ts <- input_df %>% 
-		select(unique_id, day, normalized_abundance, otu_feature) %>% 
-		filter(otu_feature %in% c(current_otu1, current_otu2)) %>% 
-		spread(otu_feature, normalized_abundance)
+	if(!file.exists(paste0(save_dir, '/temp/ccm_by_otu_', current_otu1, '_', 
+		current_otu2, '_', treatment_subset, '_first_differenced.txt'))){ 
+		# input_df <- abx_df
+		composite_ts <- input_df %>% 
+			select(unique_id, day, normalized_abundance, otu_feature) %>% 
+			filter(otu_feature %in% c(current_otu1, current_otu2)) %>% 
+			spread(otu_feature, normalized_abundance)
 
-	data_by_plot <- split(composite_ts, composite_ts$unique_id)
-	segments_end <- cumsum(sapply(data_by_plot, NROW))
-	segments_begin <- c(1, segments_end[-length(segments_end)] + 1)
-	segments <- cbind(segments_begin, segments_end) 
-	
-	best_E <- embedding_nonlinearity %>% 
-		filter(taxa %in% c(current_otu1, current_otu2)) %>% 
-		select(taxa, embedding) %>% 
-		unique
+		data_by_plot <- split(composite_ts, composite_ts$unique_id)
+		segments_end <- cumsum(sapply(data_by_plot, NROW))
+		segments_begin <- c(1, segments_end[-length(segments_end)] + 1)
+		segments <- cbind(segments_begin, segments_end) 
+		
+		best_E <- embedding_nonlinearity %>% 
+			filter(taxa %in% c(current_otu1, current_otu2)) %>% 
+			select(taxa, embedding) %>% 
+			unique
 
-	print(paste0('Beginning ', current_otu1, ' and ', current_otu2, ' in from ', treatment_subset))
+		print(paste0('Beginning ', current_otu1, ' and ', current_otu2, ' in from ', treatment_subset))
 
-	ccm_run_results <- lapply(1:500, function(i){
-		surrogate_ts <- composite_ts %>% 
-			group_by(unique_id) %>% 
-			mutate(day = c(0,sample(day[day > 0]))) %>% 
-			arrange(unique_id, day) %>% 
+		ccm_run_results <- lapply(1:500, function(i){
+			surrogate_ts <- composite_ts %>% 
+				group_by(unique_id) %>% 
+				mutate(day = c(0,sample(day[day > 0]))) %>% 
+				arrange(unique_id, day) %>% 
+				ungroup
+			pred_order <- data.frame(unique_id = sample(mouse_list, replace = T),
+				order = 1:NROW(mouse_list), stringsAsFactors = F)
+			rnd_composite_ts <- right_join(composite_ts, pred_order, by = 'unique_id')
+			rnd_surrogate_ts <- right_join(surrogate_ts, pred_order, by = 'unique_id')
+			#Run the CCM test
+			# Does B "cause" A?
+			otu1_xmap_otu2 <- ccm(rnd_composite_ts, lib = segments, pred = segments, 
+				lib_column = current_otu1, target_column = current_otu2, 
+				E = pull(filter(best_E, taxa %in% current_otu1), embedding), 
+				lib_sizes = lib_sizes, silent = TRUE)
+			# Does A "cause" B?
+			otu2_xmap_otu1 <- ccm(rnd_composite_ts, lib = segments, pred = segments, 
+				lib_column = current_otu2, target_column = current_otu1, 
+				E = pull(filter(best_E, taxa %in% current_otu2), embedding), 
+	    		lib_sizes = lib_sizes, silent = TRUE)
+			# null model with surrogate data
+			otu1_xmap_otu2_surr <- ccm(surrogate_ts, lib = segments, pred = segments, 
+				lib_column = current_otu1, target_column = current_otu2, 
+				E = pull(filter(best_E, taxa %in% current_otu1), embedding), 
+				lib_sizes = lib_sizes, silent = TRUE)
+			otu2_xmap_otu1_surr <- ccm(surrogate_ts, lib = segments, pred = segments, 
+				lib_column = current_otu2, target_column = current_otu1, 
+				E = pull(filter(best_E, taxa %in% current_otu2), embedding), 
+	    		lib_sizes = lib_sizes, silent = TRUE)
+
+			return(rbind(
+				data.frame(rbind(otu1_xmap_otu2, otu2_xmap_otu1), data = 'real'),
+				data.frame(rbind(otu1_xmap_otu2_surr, otu2_xmap_otu1_surr), data = 'surrogate'))
+				)
+			})
+
+		ccm_data <- do.call('rbind', ccm_run_results) %>% 
+			mutate_if(is.factor, as.character) %>% 
+			mutate(causal = paste0(lib_column, 'xmap', target_column))
+
+		# plot the ability of otu to predict the other otu
+		# note causal direction is opposite of xmap
+		# so if otu1 xmaps to otu2, then otu2 causes otu1
+		# this is because otu2 has left an impression on otu1 
+
+		default_rho <- cor.test(pull(composite_ts, current_otu1), pull(composite_ts, current_otu2),
+			method = "spearman", use = 'complete.obs')
+
+		ccm_plot <- ccm_data %>% 
+			ggplot(aes(x = lib_size, y = rho)) +
+				stat_summary(fun.data = 'median_hilow', geom = 'ribbon', 
+					alpha = 0.2, fun.args =(conf.int = 0.5), aes(fill = data)) + 
+				stat_summary(aes(color = data), fun.y = median, geom = 'line') + 
+				scale_color_manual(values = c('#CC0000', '#555555'), limits = c('real', 'surrogate')) +
+				scale_fill_manual(values = c('#CC0000', '#555555'), limits = c('real', 'surrogate')) + 
+				guides(color = FALSE, fill=guide_legend(title=NULL)) + 
+				labs(x = 'libray size', y = 'rho', title = 'Convergent Cross Map Analysis - Test feature interaction', 
+					subtitle = 'Surrogate data is randomly permuted time indices\nMedian (solid line) with IQR (dotted lines) and 5/95th percentile (shaded area)\n(First differenced, treatment = Antibiotic_Dose_RecoveryBeforeChallenge)') + 
+				facet_grid(causal~., scale = 'free_y') + 
+				geom_hline(yintercept = default_rho$estimate, linetype = 3) + 
+				theme_bw(base_size = 8) + theme(legend.position = c(0.1,0.9))
+
+		ggsave(filename = paste0(save_dir, '/', current_otu1, '_', current_otu2, '_ccm.jpg'), 
+			plot = ccm_plot, width = 7, height = 10, device = 'jpeg')
+		
+		# test for convergence
+			# test for significant monotonic increasing trend with rho(L) using spearman test
+			# test for significant improvement in rho(L) by wilcox test (difference min/max library)
+		print('Running ccm convergence test')
+		ccm_convergence_test <- full_join( 
+			group_by(ccm_data, causal, data) %>% 
+				summarise(ccm_trend_rho = cor.test(rho, lib_size, 
+					method = 'spearman', use = 'complete.obs')$estimate,
+					ccm_trend_p = cor.test(rho, lib_size, 
+					method = 'spearman', use = 'complete.obs')$p.value),
+			filter(ccm_data, lib_size %in% c(min(lib_size), max(lib_size))) %>%
+				group_by(causal, data) %>% 
+				summarise(median_rho_min_lobs = median(rho[lib_size == min(lib_size)], na.rm = T),
+					median_rho_max_lobs = median(rho[lib_size == max(lib_size)], na.rm = T),
+					p_min_v_max_lobs = wilcox.test(rho ~ lib_size,
+						alternative = 'less')$p.value),
+			by = c('causal','data')) %>% 
 			ungroup
-		pred_order <- data.frame(unique_id = sample(mouse_list, replace = T),
-			order = 1:NROW(mouse_list), stringsAsFactors = F)
-		rnd_composite_ts <- right_join(composite_ts, pred_order, by = 'unique_id')
-		rnd_surrogate_ts <- right_join(surrogate_ts, pred_order, by = 'unique_id')
-		#Run the CCM test
-		# Does B "cause" A?
-		otu1_xmap_otu2 <- ccm(rnd_composite_ts, lib = segments, pred = segments, 
-			lib_column = current_otu1, target_column = current_otu2, 
-			E = pull(filter(best_E, taxa %in% current_otu1), embedding), 
-			lib_sizes = lib_sizes, silent = TRUE)
-		# Does A "cause" B?
-		otu2_xmap_otu1 <- ccm(rnd_composite_ts, lib = segments, pred = segments, 
-			lib_column = current_otu2, target_column = current_otu1, 
-			E = pull(filter(best_E, taxa %in% current_otu2), embedding), 
-    		lib_sizes = lib_sizes, silent = TRUE)
-		# null model with surrogate data
-		otu1_xmap_otu2_surr <- ccm(surrogate_ts, lib = segments, pred = segments, 
-			lib_column = current_otu1, target_column = current_otu2, 
-			E = pull(filter(best_E, taxa %in% current_otu1), embedding), 
-			lib_sizes = lib_sizes, silent = TRUE)
-		otu2_xmap_otu1_surr <- ccm(surrogate_ts, lib = segments, pred = segments, 
-			lib_column = current_otu2, target_column = current_otu1, 
-			E = pull(filter(best_E, taxa %in% current_otu2), embedding), 
-    		lib_sizes = lib_sizes, silent = TRUE)
 
-		return(rbind(
-			data.frame(rbind(otu1_xmap_otu2, otu2_xmap_otu1), data = 'real'),
-			data.frame(rbind(otu1_xmap_otu2_surr, otu2_xmap_otu1_surr), data = 'surrogate'))
-			)
-		})
+		# test for significance of cross map
+		print('Running ccm significance test')
+		ccm_significance_test <- full_join(
+			group_by(ccm_data, causal, data) %>% 
+				filter(lib_size > quantile(lib_size)['75%']) %>% 
+				summarise(linear_corr_p = my.t.test.p.value(rho, 
+						mu = default_rho$estimate, alternative = 'greater')) %>% 
+				ungroup,
+			group_by(ccm_data, causal) %>% 
+				filter(lib_size > quantile(lib_size)['75%']) %>% 
+				summarise(ccm_null_p = wilcox.test(rho ~ data, 
+						alternative = 'greater')$p.value) %>% 
+				ungroup,
+			by = 'causal')
+		print('Saving ccm results')
+		write.table(full_join(ccm_convergence_test, ccm_significance_test, by = c("causal", "data")), 
+			paste0(save_dir, '/temp/ccm_by_otu_', 
+				current_otu1, '_', current_otu2, '_', treatment_subset, '_first_differenced.txt'), 
+		quote = F, row.names = F)
 
-	ccm_data <- do.call('rbind', ccm_run_results) %>% 
-		mutate_if(is.factor, as.character) %>% 
-		mutate(causal = paste0(lib_column, 'xmap', target_column))
+		print(paste0('Completed ', current_otu1, ' and ', current_otu2, ' in from ', 
+			treatment_subset))
+	} else {
+		print(paste0(current_otu1, ' and ', current_otu2, ' in from ', 
+			treatment_subset, ' already complete'))
+	}
 
-	# plot the ability of otu to predict the other otu
-	# note causal direction is opposite of xmap
-	# so if otu1 xmaps to otu2, then otu2 causes otu1
-	# this is because otu2 has left an impression on otu1 
-
-	default_rho <- cor.test(pull(composite_ts, current_otu1), pull(composite_ts, current_otu2),
-		method = "spearman", use = 'complete.obs')
-
-	ccm_plot <- ccm_data %>% 
-		ggplot(aes(x = lib_size, y = rho)) +
-			stat_summary(fun.data = 'median_hilow', geom = 'ribbon', 
-				alpha = 0.2, fun.args =(conf.int = 0.5), aes(fill = data)) + 
-			stat_summary(aes(color = data), fun.y = median, geom = 'line') + 
-			scale_color_manual(values = c('#CC0000', '#555555'), limits = c('real', 'surrogate')) +
-			scale_fill_manual(values = c('#CC0000', '#555555'), limits = c('real', 'surrogate')) + 
-			guides(color = FALSE, fill=guide_legend(title=NULL)) + 
-			labs(x = 'libray size', y = 'rho', title = 'Convergent Cross Map Analysis - Test feature interaction', 
-				subtitle = 'Surrogate data is randomly permuted time indices\nMedian (solid line) with IQR (dotted lines) and 5/95th percentile (shaded area)\n(First differenced, treatment = Antibiotic_Dose_RecoveryBeforeChallenge)') + 
-			facet_grid(causal~., scale = 'free_y') + 
-			geom_hline(yintercept = default_rho$estimate, linetype = 3) + 
-			theme_bw(base_size = 8) + theme(legend.position = c(0.1,0.9))
-
-	ggsave(filename = paste0(save_dir, '/', current_otu1, '_', current_otu2, '_ccm.jpg'), 
-		plot = ccm_plot, width = 7, height = 10, device = 'jpeg')
-	
-	# test for convergence
-		# test for significant monotonic increasing trend with rho(L) using spearman test
-		# test for significant improvement in rho(L) by wilcox test (difference min/max library)
-	print('Running ccm convergence test')
-	ccm_convergence_test <- full_join( 
-		group_by(ccm_data, causal, data) %>% 
-			summarise(ccm_trend_rho = cor.test(rho, lib_size, 
-				method = 'spearman', use = 'complete.obs')$estimate,
-				ccm_trend_p = cor.test(rho, lib_size, 
-				method = 'spearman', use = 'complete.obs')$p.value),
-		filter(ccm_data, lib_size %in% c(min(lib_size), max(lib_size))) %>%
-			group_by(causal, data) %>% 
-			summarise(median_rho_min_lobs = median(rho[lib_size == min(lib_size)], na.rm = T),
-				median_rho_max_lobs = median(rho[lib_size == max(lib_size)], na.rm = T),
-				p_min_v_max_lobs = wilcox.test(rho ~ lib_size,
-					alternative = 'less')$p.value),
-		by = c('causal','data')) %>% 
-		ungroup
-
-	# test for significance of cross map
-	print('Running ccm significance test')
-	ccm_significance_test <- full_join(
-		group_by(ccm_data, causal, data) %>% 
-			filter(lib_size > quantile(lib_size)['75%']) %>% 
-			summarise(linear_corr_p = my.t.test.p.value(rho, 
-					mu = default_rho$estimate, alternative = 'greater')) %>% 
-			ungroup,
-		group_by(ccm_data, causal) %>% 
-			filter(lib_size > quantile(lib_size)['75%']) %>% 
-			summarise(ccm_null_p = wilcox.test(rho ~ data, 
-					alternative = 'greater')$p.value) %>% 
-			ungroup,
-		by = 'causal')
-	print('Saving ccm results')
-	write.table(full_join(ccm_convergence_test, ccm_significance_test, by = c("causal", "data")), 
-		paste0(save_dir, '/temp/ccm_by_otu_', 
-			current_otu1, '_', current_otu2, '_', treatment_subset, '_first_differenced.txt'), 
-	quote = F, row.names = F)
-
-	print(paste0('Completed ', current_otu1, ' and ', current_otu2, ' in from ', 
-		treatment_subset))
 }
 
 # create function to generate NA if t test cannot calculate value
