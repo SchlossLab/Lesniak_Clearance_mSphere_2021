4050640af7e13cb4b1e40b0eee792e60c4b236af
Nick Lesniak <NLesniak@umich.edu>
2019-01-16 16:18:43 -0500

    fix code to work with use in rmd (set rownames to group col)

--- code/sum_otu_by_taxa.R
+++ code/sum_otu_by_taxa.R
@@ -11,20 +11,21 @@
 # taxa_level - genus, family, order, class, phylum, kingdom
 #
 ###################
-source('code/taxa_labels.R') # source function to get taxonomy of OTUs
+source('../code/taxa_labels.R') # source function to get taxonomy of OTUs
 
 sum_otu_by_taxa <- function(taxonomy_file, otu_df = shared_file, taxa_level = 'genus'){
   taxa_df <- get_taxa_labels(taxa_file = taxonomy_file, taxa_level = taxa_level, 
                              otu_subset = colnames(otu_df)[grep('Otu\\d', colnames(otu_df))])
-# change to read in with stringasfactor = F and eliminate as.character
+
   taxa_groups <- as.character(unique(taxa_df$taxa)) # get taxonomy groups
 # simplify following with dplyr (following is pat/matts suggestion)
-#  otu_df$sample <- rownames(otu_df)
-  output_dataframe <- otu_df %>%
+  output_dataframe <- otu_df %>% 
+    mutate(group = rownames(otu_df)) %>% 
     gather(otu, counts, contains('Otu0')) %>% 
-    inner_join(select(taxa_df, otu, taxa)) %>% 
-    group_by(Group, taxa) %>%
+    inner_join(select(taxa_df, otu, taxa), by = 'otu') %>% 
+    group_by(group, taxa) %>%
     summarize(total=sum(counts)) %>% 
-    spread(taxa, total)
+    spread(taxa, total) %>% 
+    ungroup
   return(output_dataframe)
   }
