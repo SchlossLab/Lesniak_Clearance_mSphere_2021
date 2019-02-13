63c01c970c19cc68cd39f0931a16ad89f14830d3
Nick Lesniak <NLesniak@umich.edu>
2019-02-13 13:58:44 -0500

    edit sum otu to use new taxa file format and handle other

--- code/sum_otu_by_taxa.R
+++ code/sum_otu_by_taxa.R
@@ -3,29 +3,57 @@
 # sum_otu_by_taxa.R
 #
 # created by Nicholas Lesniak in 2016
+# modified by Nicholas Lesniak in 2019
 #    
 # function outputs a dataframe with OTUs summed by taxa group per sample
 # 
-# taxonomy_file - taxonomy file location
+# taxonomy_df - dataframe formatted with convert_OTU_labels.R
 # otu_df - dataframe with samples as rownames and otus in columns
 # taxa_level - genus, family, order, class, phylum, kingdom
+# 
 #
 ###################
-source('../code/taxa_labels.R') # source function to get taxonomy of OTUs
 
-sum_otu_by_taxa <- function(taxonomy_file, otu_df = shared_file, taxa_level = 'genus'){
-  taxa_df <- get_taxa_labels(taxa_file = taxonomy_file, taxa_level = taxa_level, 
-                             otu_subset = colnames(otu_df)[grep('Otu\\d', colnames(otu_df))])
+levels <- c('Kingdom','Phylum','Class','Order','Family','Genus')
 
-  taxa_groups <- as.character(unique(taxa_df$taxa)) # get taxonomy groups
-# simplify following with dplyr (following is pat/matts suggestion)
+sum_otu_by_taxa <- function(taxonomy_df, otu_df, taxa_level = 'NA', top_n = 0){
+  
+  if(!all(is.data.frame(taxonomy_df), is.data.frame(shared_df),
+    taxa_level %in% levels)) {stop(paste0(
+      'Check to make sure you have entered a data frame for taxonomy and shared 
+      and you have selected a classification level - ',
+      paste0(levels, collapse = ', ')))}
+
+  print(paste0('Summing shared by ', taxa_level))
+
   output_dataframe <- otu_df %>% 
     mutate(group = rownames(otu_df)) %>% 
-    gather(otu, counts, contains('Otu0')) %>% 
-    inner_join(select(taxa_df, otu, taxa), by = 'otu') %>% 
+    gather(OTU, counts, contains('Otu0')) %>% 
+    inner_join(select(taxa_df, OTU, taxa = one_of(taxa_level)), by = 'OTU') %>% 
     group_by(group, taxa) %>%
-    summarize(total=sum(counts)) %>% 
-    spread(taxa, total) %>% 
-    ungroup
+    summarize(total=sum(counts))
+
+  if(top_n > 0){
+    print(paste0('Returning the top ', top_n, ' groups, all others are summed in Other'))
+
+    top_taxa <- output_dataframe %>% 
+      group_by(taxa) %>% 
+      summarise(median_abundance = mean(total, na.rm = T)) %>% 
+      top_n(top_n, median_abundance) %>% 
+      select(taxa) %>% 
+      mutate(top_taxa = taxa)
+
+    output_dataframe <- output_dataframe %>%
+      full_join(top_taxa, by = 'taxa') %>%  
+      mutate(taxa = ifelse(is.na(top_taxa), 'Other', taxa)) %>% 
+      group_by(group, taxa) %>% 
+      summarise(total = sum(total))
+      mutate(dataset = paste0('top_', top_n, '_by_', taxa_level) %>% 
+      ungroup 
+
+    } else {
+      print('Returning all groups')
+    }
+
   return(output_dataframe)
   }
