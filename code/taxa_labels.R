833a57e0321ce7479ae8917759348f7ccd0c489d
Nick Lesniak <NLesniak@umich.edu>
2019-02-13 11:26:16 -0500

    simplify taxa labeling by converting taxa file to directly useable form

--- code/taxa_labels.R
+++ code/taxa_labels.R
@@ -1,48 +1,47 @@
 ###################
 #
-# taxa_labels.R
+# convert_OTU_labels.R
 #
 # creates table of tax labels for OTUs
 # created by Nicholas Lesniak in 2016
-#    
-# get taxonomy labels from a vector of OTUs and the taxonomy file
+# updated by Nicholas Lesniak in 2019
+#
+# convert mothur output to a dataframe 
 # 
-# taxa_level - genus, family, order, class, phylum, domain
-# otu_subset - vector with OTUs
-#    when NULL, defualts to all OTUs
 # taxa_file - taxonomy file location
 #
 ###################
 
 library(dplyr)
 library(tidyr)
+library(readr)
 
-get_taxa_labels <- function(taxa_file,  taxa_level='genus', otu_subset=NULL){
-  taxonomic_classification <- c("domain", "phylum", "class", "order", "family", "genus")
-  taxa_level_num <- which(taxonomic_classification == taxa_level)
-  taxa_df <- read.table(taxa_file, header = T, stringsAsFactors=FALSE) # read in taxonomy file
-  if(is.null(otu_subset)){ # create list of OTUs
-    otu_list <- as.character(taxa_df$OTU) # if no OTUs supplied use all
-  } else {
-    otu_list <- as.character(otu_subset) # ensure supplied OTUs are read as character
-  }
-  if (taxa_level_num %in% c(1:6)){ # convert taxonomy file list to dataframe
-    otu_taxonomy <- taxa_df[taxa_df$OTU %in% otu_list,] # subset taxa_df by OTU list
-    taxonomy_df <- select(otu_taxonomy, Taxonomy, otu = OTU) %>% 
-      separate(Taxonomy,sep="\\(\\d*\\);", taxonomic_classification, extra = 'drop') %>%
-      mutate(taxa = .[ , taxa_level_num]) 
-    for (i in taxa_level_num:2){ # replace unclassified with next higher level
-      next_level <- i-1
-      unclassified <- which(taxonomy_df$taxa %in% c('unclassified', 'unclassified_unclassified'))
-      taxonomy_df[unclassified, 'taxa'] <- paste0('unclassified_', as.character(taxonomy_df[unclassified, next_level]))
-    }
-# edit output to have cleaned up OTU and italic formated taxa label
-  return(taxonomy_df %>% 
-    mutate(tax_otu_label = paste0(taxa, ' (', 
-        gsub('tu0*', 'TU ', otu),')'), # create labels
-      otu_label = paste0(gsub('tu0*', 'TU ', otu))) # create labels
-    )
-  } else { # error message if wrong taxa_level input
-    print('Error: taxa_class must be genus, family, order, class, phylum, or domain')
-  }
+taxonomy_file <- 'data/mothur/abx_time.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.0.03.cons.taxonomy'
+taxonomy_df <- read.table(taxonomy_file, sep = '\t', header = T)
+
+levels <- c('Kingdom','Phylum','Class','Order','Family','Genus')
+
+# convert mothur taxonomy string to columns
+taxonomy_df_edit <- taxonomy_df %>% 
+	separate(Taxonomy, levels, sep = '\\(\\d{2,3}\\);', extra = 'drop') %>% 
+	select(-Size) 
+# in older version of mothur unclassified are listed as unclassified
+# without information from higher level classification
+# for those cases, append with lowest identified classification
+if(any(taxonomy_df_edit$Genus == 'unclassified')){ 
+	taxonomy_df_edit <- taxonomy_df_edit %>% 
+		gather(Level, Classification, -OTU) %>% # convert to long form to group classification by OTU 
+		mutate(Level = factor(Level, levels))  %>% # order classification level
+		left_join(group_by(., OTU) %>% # create dataframe with OTU and lowest level classification
+				filter(Classification != 'unclassified') %>% # remove unclassifieds
+				filter(Level == levels[max(as.numeric(Level))]) %>% # select lowest level classification
+				select(OTU, Lowest_classified = Classification), 
+			by = 'OTU') %>% 
+		mutate(Classification = ifelse(Classification == 'unclassified', 
+				paste(Lowest_classified, Classification, sep= '_'), # append unclassified with lowest classification
+				Classification)) %>% 
+		select(-Lowest_classified) %>% 
+		spread(Level, Classification)
 }
+
+write_tsv(taxonomy_df_edit, 'data/process/abx_cdiff_taxonomy_clean.tsv')
