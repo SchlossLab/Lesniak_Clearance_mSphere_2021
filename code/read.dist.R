81dfe8fa0f866ccf1657541e9fa3f5589c122652
Nick Lesniak <NLesniak@umich.edu>
2019-11-07 17:47:54 -0500

    convert read dist to tidy
    cloned function from marc sze repo

--- code/read.dist.R
+++ code/read.dist.R
@@ -2,40 +2,31 @@
 #
 # read.dist.R
 #
-#reads triangle matrices into R
-#created by Dr. Nielson Baxter in 2015
+# reads triangle matrices into R
+# 
+# cloned from Marc Sze
+# https://github.com/SchlossLab/Sze_PCRSeqEffects_mSphere_2019/blob/master/code/mock_beta.R
 #    
 #
 ###################
 
 
-read.dist <- function(file, input='lt', make.square=T, diag=0){
-  if(input=='lt'){
-    stuff <- scan(file, what='character') #gets all of the elements of the file
-    n <- as.numeric(stuff[1]) # n = number of groups/samples in file
-    stuff <- stuff[-1] # removes number of groups from list of stuff
-    m <- data.frame(matrix(NA, nrow=n, ncol=n) ) #makes empty matrix based on number of groups
-    diag(m) <- diag #fills in diagnol with specified value
-    
-    c <- 1 # c keeps track of postion in stuff vector
-    for(i in 1:n){
-      group <- stuff[c] #get group name
-      colnames(m)[i] <- group
-      rownames(m)[i] <- group
-      
-      if(i > 1){
-        m[i,1:(i-1)] <- stuff[(c+1):(c+i-1)] #fills in matrix with values from stuff
-      }
-      c<-c+i #this because math
-    }
-    if(make.square){
-      m[upper.tri(m)] <- t(m)[upper.tri(m)] #fills in upper triangle
-    }
-  }
-  
-  if(input=='square'){ #reads in square matrix
-    m<-read.table(file, skip=1, row.names=1)
-    colnames(m) <- rownames(m)
-  }
-  return(m)
+library(tidyverse)
+
+read_dist <- function(dist_file_name){
+  # read in all the data from the lower triangle (exlcude the first which is the matrix dim)
+  dist_data <- scan(dist_file_name, what="character", quiet=TRUE)[-1]
+  # subset the sample names (which all contain "-")
+  samples <- str_subset(dist_data, "-")
+  n_samples <- length(samples)
+  distance_strings <- str_subset(dist_data, "\\.")
+
+  distance_matrix <- matrix(0, nrow=n_samples, ncol=n_samples)
+  colnames(distance_matrix) <- samples
+  as.tibble(cbind(rows=samples, distance_matrix)) %>%
+    gather(columns, distances, -rows) %>%
+    filter(rows < columns) %>%
+    arrange(columns, rows) %>%
+    mutate(distances = as.numeric(distance_strings))
+
 }
