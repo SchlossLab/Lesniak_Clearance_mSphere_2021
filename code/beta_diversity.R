ff05465ca1b05a2ab7f8e66417034e7ccdd2517d
Nick Lesniak <NLesniak@umich.edu>
2019-11-07 17:49:24 -0500

    setup comparison of beta diversity

--- code/beta_diversity.R
+++ code/beta_diversity.R
@@ -1,66 +1,77 @@
-library(ggplot2)
-library(rgl)
-library(magick)
+library(tidyverse)
+library(cowplot)
 
-cfu_labels <- read.table('data/mothur/CFU.design', header = T, sep = '\t')
-cdiff_labels <- read.table('data/mothur/cdiff.design', header = T, sep = '\t')
-abx_labels <- read.table('data/mothur/abx.design', header = T, sep = '\t')
-cage_labels <- read.table('data/mothur/cage.design', header = T, sep = '\t')
-day_labels <- read.table('data/mothur/day.design', header = T, sep = '\t')
+# file names relative to code directory for Rmd
+meta_file   <- 'data/process/abx_cdiff_metadata_clean.txt'
+dist_file <- 'data/mothur/abx_time.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.thetayc.0.03.lt.ave.dist'
+dist_function <- 'code/read.dist.R'
 
-# read in nmds axes file produced by mothur
-time_nmds_3m <- read.table('data/mothur/abx_time.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.thetayc.0.03.lt.ave.nmds.3.axes', sep = '\t', header = T)
+# read in data
+meta_file   <- read.table(meta_file, sep = '\t', header = T, stringsAsFactors = F) %>% 
+	filter(abx %in% c('Clindamycin', 'Streptomycin', 'Cefoperazone'))
 
-#merge nmds with metadata
-nmds_3 <- merge(time_nmds_3m, 
-	merge(cfu_labels,
-		merge(abx_labels,
-			merge(cdiff_labels,
-				merge(cage_labels, day_labels)))))
-nmds_3$CFU[is.na(nmds_3$CFU)] <- 0
-nmds_3$abx[nmds_3$abx == 'vanc '] <- 'vanc'
-nmds_3$abx <- as.character(nmds_3$abx)
-nmds_3$dayplot <- nmds_3$day
+source(dist_function)
+tyc_df <- read_dist(dist_file)
 
-nmds_3$dayplot[nmds_3$day < 0] <- -1
+# for control variation, determine variation between samples on initial day
+initial_intra_cage <- meta_file %>% 
+	filter(time_point == 'Initial') %>% 
+	select(group, abx,clearance)	
 
-nmds_3$color <- as.numeric(factor(as.character(nmds_3$abx)))
-nmds_3$size <- as.numeric(round(c(log10(nmds_3$CFU +1) + 1), 0))
+initial_tyc <- tyc_df %>% 
+	inner_join(initial_intra_cage, by = c('rows' = 'group')) %>% 
+	inner_join(select(initial_intra_cage, group), by = c('columns' = 'group')) %>% 
+	mutate(rows = gsub('D.*', '', rows),
+		columns = gsub('D.*', '', columns)) %>% 
+	separate(rows, c('cage_rows', 'mouse_rows')) %>% 
+	separate(columns, c('cage_cols', 'mouse_cols'))
+# compare mice against other cages at inital
+initial_inter_cage_diversity <- initial_tyc %>% 
+	filter(cage_rows != cage_cols) %>% 
+	mutate(comparison = 'Between Cages') %>% 
+	select(comparison, clearance, abx, tyc = distances)
+# compare mice within cage at initial
+initial_intra_cage_diversity <- initial_tyc %>% 
+	filter(cage_rows == cage_cols) %>% 
+	mutate(comparison = 'Within Cages') %>% 
+	select(comparison, clearance, abx, tyc = distances)
 
-# plot nmds
-# plot in 3D
-day_list <- split(nmds_3, nmds_3$dayplot)
-abx_list <- split(nmds_3, nmds_3$abx)
-cfu_df <- nmds_3[nmds_3$day > 0, ]
-par3d(windowRect = c(0, 0, 700, 700))
-with(nmds_3, 
-	points3d(axis1, axis2, axis3, 
-		#xlab = 'axis_1', ylab = 'axis_2', zlab = 'axis_3',
-		width = 1000, size=NULL))
-for(sz in c(1:10)){
-	with(cfu_df[cfu_df$size == sz, ], 
-			points3d(axis1, axis2, axis3, 
-				#xlab = 'axis_1', ylab = 'axis_2', zlab = 'axis_3',
-				width = 1000, size = sz)
-				)
-		#writeOBJ(paste0('test_abx_', unique(nmds_3$abx)[i], '.obj'), 
-		#	#separateObjects = T
-		#	pointRadius = c(seq(10, 40, 3.33)/1000)[sz]
-		#	)
+compare_time_point <- function(timepoints){
+	samples_list <- meta_file %>% 
+		filter(time_point %in% timepoints) %>% 
+		select(group, clearance, abx)	
+
+	output_diversity <- tyc_df %>% 
+		inner_join(samples_list, by = c('rows' = 'group')) %>% 
+		inner_join(select(samples_list, group), by = c('columns' = 'group')) %>% 
+		mutate(rows = gsub('D.*', '', rows),
+			columns = gsub('D.*', '', columns)) %>% 
+		filter(rows == columns) %>% 
+		mutate(comparison = paste(timepoints, collapse = " to ")) %>% 
+		select(comparison, clearance, abx, tyc = distances)
+	return(output_diversity)
 }
 
-bgplot3d({
-  plot.new()
-  title(main = 'Antibiotic-treated conventional mice challenged with C. difficile', line = 3)
-  mtext(side = 1, 'Points are sized by colonization level',
-  	 line = 4, cex = 2)
-  legend("topright", cex = 2,
-  	pch=c(16,16,16,16), pt.cex = c(0.6,0.9,1.2,1.5), inset=c(0.02),
-  	legend = c(expression(paste("10"^"2")), expression(paste("10"^"4")),
-  		expression(paste("10"^"6")), expression(paste("10"^"8")))
-  	)
-})
 
-#writeOBJ('test_legend.obj')
-movie3d(spin3d(axis=c(0,0,1), rpm=3), dir = 'scratch/nmds3d/', duration=20, fps=10, movie="nmds3d_plot_by_cfu")
-writeWebGL(filename = 'scratch/nmds3d/nmds_3d_by_cfu.html')
+
+# compare mice on day 0 to initial
+# compare day day 0 to end
+# compare intial to end
+comparisons <- list(c('Day 0', 'Initial'), c('Day 0', 'End'), c('End', 'Initial'))
+tyc_comparisons <- map_dfr(comparisons, ~ compare_time_point(.x))
+
+tyc_df <- rbind(tyc_comparisons, initial_intra_cage_diversity, initial_inter_cage_diversity) 
+
+tyc_df %>% 
+	ggplot(aes(x = comparison, y = tyc)) +
+		geom_boxplot() + 
+		theme_bw() + 
+		facet_wrap(abx~.)
+
+tyc_df %>% 
+	mutate(comparison = factor(tyc_df$comparison, 
+		levels = c('Within Cages', 'Between Cages', 'Day 0 to Initial', 'Day 0 to End', 'End to Initial'))) %>% 
+	filter(clearance != 'Clearing') %>% 
+	ggplot(aes(x = comparison, y = tyc, fill = clearance)) +
+		geom_boxplot() + 
+		theme_bw()
