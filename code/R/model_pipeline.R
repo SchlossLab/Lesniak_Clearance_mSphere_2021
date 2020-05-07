4ce726433e7ba7e3bfa1cee27af2cfd333ede0df
Nick Lesniak <NLesniak@umich.edu>
2020-05-07 12:00:54 -0400

    create correlation matrix for each data set run

--- code/R/model_pipeline.R
+++ code/R/model_pipeline.R
@@ -183,7 +183,7 @@ pipeline <- function(data, model, split_number, outcome=NA, hyperparameters=NULL
       #     1. Predict held-out test-data
       #     2. Calculate ROC and AUROC values on this prediction
       #     3. Get the feature importances for correlated and uncorrelated feautures
-      roc_results <- permutation_importance(trained_model, test_data, first_outcome, second_outcome, outcome)
+      roc_results <- permutation_importance(trained_model, test_data, first_outcome, second_outcome, outcome, level)
       test_auc <- roc_results[[1]]  # Predict the base test importance
       feature_importance_non_cor <- roc_results[2] # save permutation results
       # Get feature weights
@@ -197,7 +197,7 @@ pipeline <- function(data, model, split_number, outcome=NA, hyperparameters=NULL
       #     1. Predict held-out test-data
       #     2. Calculate ROC and AUROC values on this prediction
       #     3. Get the feature importances for correlated and uncorrelated feautures
-      roc_results <- permutation_importance(trained_model, test_data, first_outcome, second_outcome, outcome)
+      roc_results <- permutation_importance(trained_model, test_data, first_outcome, second_outcome, outcome, level)
       test_auc <- roc_results[[1]] # Predict the base test importance
       feature_importance_non_cor <- roc_results[2] # save permutation results of non-cor
       feature_importance_cor <- roc_results[3] # save permutation results of cor
